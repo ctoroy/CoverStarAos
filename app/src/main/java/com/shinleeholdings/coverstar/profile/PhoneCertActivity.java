5ec1b1d6f077febc764591771eb918dc74048c61
hihi0677 <hihi0677@gmail.com>
2022-04-21 02:43:02 +0900

    국가선택 작업

--- app/src/main/java/com/shinleeholdings/coverstar/profile/PhoneCertActivity.java
+++ app/src/main/java/com/shinleeholdings/coverstar/profile/PhoneCertActivity.java
@@ -4,6 +4,8 @@ import static com.shinleeholdings.coverstar.util.LoginHelper.PHONE_CERT_MODE_JOI
 import static com.shinleeholdings.coverstar.util.LoginHelper.PHONE_CERT_MODE_LOGIN;
 import static com.shinleeholdings.coverstar.util.LoginHelper.PHONE_CERT_MODE_RECERT;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.text.TextUtils;
@@ -11,6 +13,7 @@ import android.view.View;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 
 import com.google.firebase.FirebaseException;
 import com.google.firebase.FirebaseTooManyRequestsException;
@@ -20,9 +23,9 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.PhoneAuthCredential;
 import com.google.firebase.auth.PhoneAuthOptions;
 import com.google.firebase.auth.PhoneAuthProvider;
+import com.hbb20.CountryCodePicker;
 import com.shinleeholdings.coverstar.AppConstants;
 import com.shinleeholdings.coverstar.R;
-import com.shinleeholdings.coverstar.data.CountryData;
 import com.shinleeholdings.coverstar.databinding.ActivityPhoneCertBinding;
 import com.shinleeholdings.coverstar.util.BaseActivity;
 import com.shinleeholdings.coverstar.util.DebugLogger;
@@ -39,8 +42,6 @@ public class PhoneCertActivity extends BaseActivity {
     private String certMode = "";
 
     private ActivityPhoneCertBinding binding;
-
-    private CountryData selectedCountry;
 
     CountDownTimer cTimer;
 
@@ -66,17 +67,34 @@ public class PhoneCertActivity extends BaseActivity {
         setModeUi();
     }
 
+    private void showCCP() {
+        binding.ccp.setDialogEventsListener(new CountryCodePicker.DialogEventsListener() {
+            @Override
+            public void onCcpDialogOpen(Dialog dialog) {
+                CardView rootCardView = dialog.getWindow().getDecorView().findViewById(com.hbb20.R.id.cardViewRoot);
+                rootCardView.getLayoutParams().height = (int) Util.convertDpToPixel(626f, PhoneCertActivity.this);
+            }
+
+            @Override
+            public void onCcpDialogDismiss(DialogInterface dialogInterface) {
+
+            }
+
+            @Override
+            public void onCcpDialogCancel(DialogInterface dialogInterface) {
+
+            }
+        });
+
+        binding.ccp.setOnCountryChangeListener(() -> setCountrySelected());
+
+        binding.ccp.launchCountrySelectionDialog();
+    }
+
     private void initUi() {
         binding.titleLayout.titleBackLayout.setOnClickListener(view -> onBackPressed());
 
-        binding.selectCountryLayout.setOnClickListener(view ->
-            // TODO test
-//                binding.ccp.performClick();
-            DialogHelper.showCountrySelectPopup(PhoneCertActivity.this, country -> {
-                selectedCountry = country;
-                setCountrySelected();
-            })
-        );
+        binding.selectCountryLayout.setOnClickListener(view -> showCCP());
 
         binding.nextButton.setOnClickListener(view -> {
             if (certMode.equals(PHONE_CERT_MODE_JOIN)) {
@@ -93,8 +111,6 @@ public class PhoneCertActivity extends BaseActivity {
         if (certMode.equals(PHONE_CERT_MODE_RECERT)){
             binding.titleLayout.titleTextView.setText(R.string.login);
             binding.phoneCertNeed.setText(Util.getSectionOfTextBold(getString(R.string.insert_registered_num), getString(R.string.registered_num)));
-            // TODO 기본 국가정보 세팅
-            selectedCountry = new CountryData("한국", "+82", "");
             binding.loginHintTextView.setVisibility(View.GONE);
             binding.userPhotoLayout.setVisibility(View.GONE);
             binding.nextButton.setText(getString(R.string.next));
@@ -103,7 +119,7 @@ public class PhoneCertActivity extends BaseActivity {
             // TODO 저장된 사용자 정보 세팅(닉네임, 휴대폰, 사진경로, 국가번호)
             String nickName = "테스트";
             String phoneNum = "01031240677";
-            selectedCountry = new CountryData("한국", "+82", "");
+            // TODO 저장된 국가정보 ccp에 세팅
 
             String message = String.format(getString(R.string.welcome_user), nickName);
             binding.phoneCertNeed.setText(Util.getSectionOfTextBold(message, nickName));
@@ -115,8 +131,6 @@ public class PhoneCertActivity extends BaseActivity {
             binding.titleLayout.titleTextView.setText(R.string.phone_cert);
             binding.phoneCertNeed.setText(Util.getSectionOfTextBold(getString(R.string.phone_cert_agree_need), getString(R.string.phone_cert)));
 
-            // TODO 기본 국가정보 세팅
-            selectedCountry = new CountryData("한국", "+82", "");
             binding.loginHintTextView.setVisibility(View.GONE);
             binding.userPhotoLayout.setVisibility(View.GONE);
             binding.nextButton.setText(getString(R.string.send_phone_auth));
@@ -126,7 +140,11 @@ public class PhoneCertActivity extends BaseActivity {
     }
 
     private void setCountrySelected() {
-        binding.selectedCountryTextView.setText(selectedCountry.getName() + selectedCountry.getCode());
+        String countryName = binding.ccp.getSelectedCountryName();
+        String countryCode = binding.ccp.getSelectedCountryCodeWithPlus();
+        String countryNameCode = binding.ccp.getSelectedCountryNameCode();
+        DebugLogger.i("selectedCountry : " + countryName + ", " + countryCode + " , " + countryNameCode);
+        binding.selectedCountryTextView.setText(countryName + countryCode);
     }
 
     private void showCertNumInputLayout() {
@@ -170,51 +188,51 @@ public class PhoneCertActivity extends BaseActivity {
 
 //        https://firebase.google.com/docs/auth/android/phone-auth?hl=ko&authuser=0
         initAuthInfo();
-        // TODO 사용자의 국가 설정
-//        mFirebaseAuth.setLanguageCode("kr");
+
+        mFirebaseAuth.setLanguageCode(binding.ccp.getSelectedCountryNameCode());
         ProgressDialogHelper.show(this);
         PhoneAuthOptions options = PhoneAuthOptions.newBuilder(mFirebaseAuth)
-                        .setPhoneNumber("+8201031240677")       // Phone number to verify
-                        .setTimeout(0L, TimeUnit.SECONDS) // Timeout and unit
-                        // disable "auto-retrieval" by setting the timeout of verifyPhoneNumber to 0
-                        // (Reference: https://firebase.google.com/docs/reference/android/com/google/firebase/auth/PhoneAuthProvider.html)
-                        .setActivity(this)                 // Activity (for callback binding)
-                        .setCallbacks(new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
-                            @Override
-                            public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
+            .setPhoneNumber("+8201031240677")       // Phone number to verify
+            .setTimeout(0L, TimeUnit.SECONDS) // Timeout and unit
+            // disable "auto-retrieval" by setting the timeout of verifyPhoneNumber to 0
+            // (Reference: https://firebase.google.com/docs/reference/android/com/google/firebase/auth/PhoneAuthProvider.html)
+            .setActivity(this)                 // Activity (for callback binding)
+            .setCallbacks(new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+                @Override
+                public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
 //                                즉시 인증(Instant verification) : 때에 따라서는 인증 코드를 보내거나 입력하지 않고 전화번호를 즉시 인증
 //                                자동 검색(Auto-retrieval) : 일부 기기에서는 수신되는 인증 SMS를 Google Play 서비스가 자동으로 감지하여 사용자의 개입 없이 인증을 수행
 //                                일부 이동통신사에서는 이 기능을 제공하지 않을 수 있습니다. SMS 메시지 끝에 11자리 해시를 포함하는 SMS Retriever API를 사용
-                                ProgressDialogHelper.dismiss();
-                                DebugLogger.i("PhoneAuth OnVerificationStateChangedCallbacks Complete : " + phoneAuthCredential);
-                                PhoneCertActivity.this.authCredential = phoneAuthCredential;
-                                // 이미 코드 설정까지 된거라 이걸로 로그인?
-                                startFirebaseUserAuth();
-                            }
+                    ProgressDialogHelper.dismiss();
+                    DebugLogger.i("PhoneAuth OnVerificationStateChangedCallbacks Complete : " + phoneAuthCredential);
+                    PhoneCertActivity.this.authCredential = phoneAuthCredential;
+                    // 이미 코드 설정까지 된거라 이걸로 로그인?
+                    startFirebaseUserAuth();
+                }
 
-                            @Override
-                            public void onVerificationFailed(@NonNull FirebaseException e) {
-                                ProgressDialogHelper.dismiss();
+                @Override
+                public void onVerificationFailed(@NonNull FirebaseException e) {
+                    ProgressDialogHelper.dismiss();
 
-                                DebugLogger.i("PhoneAuth OnVerificationStateChangedCallbacks fail : " + e.getMessage());
-                                // TODO 에러 문구 노출 필요
-                                if (e instanceof FirebaseAuthInvalidCredentialsException) {
-                                } else if (e instanceof FirebaseTooManyRequestsException) {
-                                }
-                            }
+                    DebugLogger.i("PhoneAuth OnVerificationStateChangedCallbacks fail : " + e.getMessage());
+                    // TODO 에러 문구 노출 필요
+                    if (e instanceof FirebaseAuthInvalidCredentialsException) {
+                    } else if (e instanceof FirebaseTooManyRequestsException) {
+                    }
+                }
 
-                            @Override
-                            public void onCodeSent(@NonNull String verificationId, @NonNull PhoneAuthProvider.ForceResendingToken token) {
-                                ProgressDialogHelper.dismiss();
+                @Override
+                public void onCodeSent(@NonNull String verificationId, @NonNull PhoneAuthProvider.ForceResendingToken token) {
+                    ProgressDialogHelper.dismiss();
 
-                                DebugLogger.i("PhoneAuth OnVerificationStateChangedCallbacks onCodeSent");
-                                PhoneCertActivity.this.verificationId = verificationId;
-                                PhoneCertActivity.this.token = token;
-                                DialogHelper.showCertNumSendCompletePopup(PhoneCertActivity.this);
-                                showCertNumInputLayout();
-                            }
-                        })
-                        .build();
+                    DebugLogger.i("PhoneAuth OnVerificationStateChangedCallbacks onCodeSent");
+                    PhoneCertActivity.this.verificationId = verificationId;
+                    PhoneCertActivity.this.token = token;
+                    DialogHelper.showCertNumSendCompletePopup(PhoneCertActivity.this);
+                    showCertNumInputLayout();
+                }
+            })
+            .build();
         PhoneAuthProvider.verifyPhoneNumber(options);
     }
 
--- app/src/main/java/com/shinleeholdings/coverstar/util/DialogHelper.java
+++ app/src/main/java/com/shinleeholdings/coverstar/util/DialogHelper.java
@@ -5,22 +5,13 @@ import android.app.Dialog;
 import android.content.Context;
 import android.view.Gravity;
 import android.view.LayoutInflater;
-import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
-import com.hbb20.CountryCodePicker;
 import com.shinleeholdings.coverstar.R;
-import com.shinleeholdings.coverstar.data.CountryData;
-import com.shinleeholdings.coverstar.ui.CountryListAdapter;
-
-import java.util.ArrayList;
 
 public class DialogHelper {
     private static final boolean DIALOG_CANCELABLE = false;
@@ -48,58 +39,6 @@ public class DialogHelper {
         countryListView.setText(Util.getSectionOfTextColor(activity, R.color.color_f45786, activity.getString(R.string.cert_num_send_complete), activity.getString(R.string.send)));
         Button okButton = view.findViewById(R.id.okButton);
         okButton.setOnClickListener(view12 -> dialog.dismiss());
-        dialog.show();
-    }
-
-
-    public interface ICountrySelectListener {
-        public void onCountrySelected(CountryData country);
-    }
-
-    public static void test(Activity activity) {
-
-    }
-
-    public static void showCountrySelectPopup(Activity activity, ICountrySelectListener listener) {
-        // TODO 국가 설정
-        ArrayList<CountryData> countryDataArrayList = new ArrayList<>();
-        countryDataArrayList.add(new CountryData("한국1", "+82", ""));
-        countryDataArrayList.add(new CountryData("한국2", "+82", ""));
-        countryDataArrayList.add(new CountryData("한국3", "+82", ""));
-        countryDataArrayList.add(new CountryData("한국4", "+82", ""));
-        countryDataArrayList.add(new CountryData("한국5", "+82", ""));
-        countryDataArrayList.add(new CountryData("한국6", "+82", ""));
-        countryDataArrayList.add(new CountryData("한국7", "+82", ""));
-        countryDataArrayList.add(new CountryData("한국8", "+82", ""));
-        countryDataArrayList.add(new CountryData("한국9", "+82", ""));
-        countryDataArrayList.add(new CountryData("한국10", "+82", ""));
-        countryDataArrayList.add(new CountryData("한국11", "+82", ""));
-        countryDataArrayList.add(new CountryData("한국12", "+82", ""));
-        countryDataArrayList.add(new CountryData("한국13", "+82", ""));
-        countryDataArrayList.add(new CountryData("한국14", "+82", ""));
-        countryDataArrayList.add(new CountryData("한국15", "+82", ""));
-        countryDataArrayList.add(new CountryData("한국16", "+82", ""));
-        countryDataArrayList.add(new CountryData("한국17", "+82", ""));
-        countryDataArrayList.add(new CountryData("한국18", "+82", ""));
-
-        final Dialog dialog = new Dialog(activity, R.style.Theme_TransparentBackground);
-        ViewGroup view = (ViewGroup) LayoutInflater.from(activity).inflate(R.layout.layout_country_select, null);
-        dialog.setContentView(view);
-
-        FrameLayout closeLayout = view.findViewById(R.id.closeLayout);
-        closeLayout.setOnClickListener(view1 -> dialog.dismiss());
-
-
-        CountryListAdapter adapter = new CountryListAdapter();
-        adapter.setItemList(countryDataArrayList, country -> {
-            listener.onCountrySelected(country);
-            dialog.dismiss();
-        });
-
-        RecyclerView countryListView = view.findViewById(R.id.countryRecyclerView);
-        countryListView.setLayoutManager(new LinearLayoutManager(activity));
-        countryListView.setAdapter(adapter);
-
         dialog.show();
     }
 }
