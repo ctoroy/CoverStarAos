8c07c5bf27dd701bf9de67c42f9e73438ffce233
kevin <hihi0677@truefriend.com>
2022-05-16 17:52:23 +0900

    123

--- app/src/main/java/com/shinleeholdings/coverstar/data/AlarmItem.java
+++ app/src/main/java/com/shinleeholdings/coverstar/data/AlarmItem.java
@@ -1,6 +1,17 @@
 package com.shinleeholdings.coverstar.data;
 
-public class AlarmItem {
+import io.realm.RealmObject;
+
+public class AlarmItem extends RealmObject {
+    //        @PrimaryKey
+//        @SerializedName("PDNO")
+//        open var scode: String = "" // 종목 코드
+//
+//        @SerializedName("STOCK_ID")
+//        open var stockId: Int = 0
+//
+//        @SerializedName("KEYWORDS")
+//        open var keywordList: RealmList<String> = RealmList()
 
     public boolean isSelected = false;
 }
---
+++ app/src/main/java/com/shinleeholdings/coverstar/util/AlarmHelper.java
@@ -0,0 +1,58 @@
+package com.shinleeholdings.coverstar.util;
+
+import android.os.Debug;
+
+import com.shinleeholdings.coverstar.data.AlarmItem;
+
+import java.util.ArrayList;
+
+import io.realm.Realm;
+import io.realm.RealmConfiguration;
+
+public class AlarmHelper {
+    private static final String TAG = "AlarmHelper";
+    private static volatile AlarmHelper instance;
+    private final static Object lockObject = new Object();
+    public static AlarmHelper getSingleInstance() {
+        if (instance == null) {
+            synchronized (lockObject) {
+                if (instance == null) {
+                    instance = new AlarmHelper();
+                }
+            }
+        }
+
+        return instance;
+    }
+
+
+    public ArrayList<AlarmItem> getAlarmList() {
+        DebugLogger.i(TAG, "alarmHelper getAlarmList");
+
+        RealmConfiguration config = new RealmConfiguration.Builder().allowWritesOnUiThread(true).allowQueriesOnUiThread(true).build();
+        Realm defaultRealm = Realm.getInstance(config);
+
+        ArrayList<AlarmItem> itemList = new ArrayList<>(defaultRealm.copyFromRealm(defaultRealm.where(AlarmItem.class).findAll()));
+        DebugLogger.i(TAG, "alarmHelper getAlarmList result : " + itemList.size());
+        return itemList;
+
+//        if (keywordResult.keywordDataList != null && keywordResult.keywordDataList!!.size > 0) {
+//        defaultRealm.executeTransaction {
+//            defaultRealm.insertOrUpdate(keywordResult.keywordDataList!!)
+//        }
+    }
+
+    public void addAlarmItem(AlarmItem item) {
+        DebugLogger.i(TAG, "alarmHelper addAlarmItem start");
+        RealmConfiguration config = new RealmConfiguration.Builder().allowWritesOnUiThread(true).allowQueriesOnUiThread(true).build();
+        Realm defaultRealm = Realm.getInstance(config);
+        defaultRealm.executeTransaction(new Realm.Transaction() {
+            @Override
+            public void execute(Realm realm) {
+                DebugLogger.i(TAG, "alarmHelper addAlarmItem insertOrUpdate start");
+                realm.insertOrUpdate(item);
+                DebugLogger.i(TAG, "alarmHelper addAlarmItem insertOrUpdate finished");
+            }
+        });
+    }
+}
