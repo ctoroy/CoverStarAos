ab206a244855007ee84d020972bdb6b6dfb5ffe3
kevin <hihi0677@truefriend.com>
2022-05-30 17:00:01 +0900

    앱 update 체크 로직 추가

--- app/src/main/java/com/shinleeholdings/coverstar/SplashActivity.java
+++ app/src/main/java/com/shinleeholdings/coverstar/SplashActivity.java
@@ -1,15 +1,26 @@
 package com.shinleeholdings.coverstar;
 
 import android.content.Intent;
+import android.content.pm.PackageInfo;
 import android.os.Bundle;
 import android.os.Handler;
 
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.play.core.appupdate.AppUpdateInfo;
+import com.google.android.play.core.appupdate.AppUpdateManager;
+import com.google.android.play.core.appupdate.AppUpdateManagerFactory;
+import com.google.android.play.core.install.model.UpdateAvailability;
 import com.google.firebase.messaging.FirebaseMessaging;
 import com.shinleeholdings.coverstar.databinding.ActivitySplashBinding;
 import com.shinleeholdings.coverstar.profile.LaunchActivity;
 import com.shinleeholdings.coverstar.util.BaseActivity;
+import com.shinleeholdings.coverstar.util.DebugLogger;
 import com.shinleeholdings.coverstar.util.LoginHelper;
 import com.shinleeholdings.coverstar.util.SharedPreferenceHelper;
+import com.shinleeholdings.coverstar.util.Util;
 
 public class SplashActivity extends BaseActivity {
 
@@ -32,7 +43,33 @@ public class SplashActivity extends BaseActivity {
                 SharedPreferenceHelper.getInstance().setSharedPreference(SharedPreferenceHelper.PUSH_ID, task.getResult());
             }
 
-            new Handler().postDelayed(this::startAutoLogin, 1000);
+            checkAppUpdate();
+        });
+    }
+
+    private void checkAppUpdate() {
+        DebugLogger.i("appUpdateInfo checkMarketStoreVersion");
+        AppUpdateManager appUpdateManager = AppUpdateManagerFactory.create(this);
+        appUpdateManager.getAppUpdateInfo().addOnCompleteListener(new OnCompleteListener<AppUpdateInfo>() {
+            @Override
+            public void onComplete(@NonNull Task<AppUpdateInfo> task) {
+                DebugLogger.i("appUpdateInfo onCompleted isComplete : " + task.isComplete() + ", isSuccessful : " + task.isSuccessful());
+                try {
+                    if (task.isComplete() && task.isSuccessful()) {
+                        PackageInfo pInfo = getApplicationContext().getPackageManager().getPackageInfo(getPackageName(), 0);
+                        int currentVersionCode = pInfo.versionCode;
+
+                        AppUpdateInfo info = task.getResult();
+                        DebugLogger.i("appUpdateInfo availableVersionCode : " + info.availableVersionCode() + ", updateAvailability : " + info.updateAvailability());
+                        if (info.availableVersionCode() > currentVersionCode || info.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE) {
+                            Util.goStore(SplashActivity.this);
+                            return;
+                        }
+                    }
+                } catch (Exception e) {
+                }
+                new Handler().postDelayed(() -> startAutoLogin(), 1000);
+            }
         });
     }
 
